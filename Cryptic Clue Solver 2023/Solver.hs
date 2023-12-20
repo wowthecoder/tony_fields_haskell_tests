6d8adbc5de84588eadf0aed184b4fe8b7832d1a1
Andrew Loo <82577844+wowthecoder@users.noreply.github.com>
2023-12-20 18:47:26 +0000

    Completed 2023 Exam

--- Cryptic Clue Solver 2023/Solver.hs
+++ Cryptic Clue Solver 2023/Solver.hs
@@ -7,6 +7,8 @@ import Types
 import WordData
 import Clues
 import Examples
+import Data.Maybe (fromMaybe)
+import GHC.Plugins (addBootSuffix)
 
 ------------------------------------------------------
 -- Part I
@@ -47,21 +49,29 @@ split3M xs
 
 matches :: String -> ParseTree -> Bool
 matches def (Synonym syn) 
-  = let syns = synonyms syn
-     in not (null syns) && def `elem` syns
+  = def `elem` synonyms syn
 matches target (Anagram _ ang) 
   = sort target == sort ang
 matches target (Reversal _ subt) 
   = matches (reverse target) subt
 matches target (Insertion _ left right)
- = or [(matches mid left) && (matches sides right) | (mid, sides) <- uninsert target]
+  = or [(matches mid left) && (matches sides right) | (mid, sides) <- uninsert target]
 matches target (Charade _ left right) 
   = or [(matches s1 left) && (matches s2 right) | (s1, s2) <- split2 target]
+matches target (HiddenWord _ hw)
+  | len == 1  = target `isInfixOf` hw 
+  | len == 2  = target `isInfixOf` combined && not (target `isInfixOf` (head ws)) && not (target `isInfixOf` (last ws))
+  | otherwise = target `isInfixOf` combined && (concat . tail . init) ws `isInfixOf` target
+  where 
+    ws = words hw
+    len = length ws
+    combined = concat ws
 
 evaluate :: Parse -> Int -> [String]
-evaluate (def, _, pTree) len = [ans | ans <- words, matches ans pTree]
-  where 
-    words = filter (\x -> length x == len) (synonyms $ concat def)
+-- evaluate (def, _, pTree) len = [ans | ans <- words, matches ans pTree]
+--   where 
+--     words = filter (\x -> length x == len) (synonyms $ unwords def)
+evaluate (d, _, t) l = filter (\s -> s `matches` t && length s == l) (synonyms (unwords d))
 
 ------------------------------------------------------
 -- Part III
@@ -73,30 +83,58 @@ parseWordplay ws
             parseAnagram ws,
             parseReversal ws,
             parseInsertion ws,
-            parseCharade ws]
+            parseCharade ws,
+            parseHiddenWord ws]
     
 parseSynonym :: [String] -> [ParseTree]
 parseSynonym ws
   | not (null $ synonyms w) = [Synonym w]
   | otherwise               = []
   where 
-    w = concat ws
+    w = unwords ws
+
+oneArg :: [String] -> [String] -> ParseTree -> [ParseTree]
+oneArg inds arg typ
+  | Just (ind, ws) <- pair = case typ of 
+    Anagram {}    -> [Anagram ind (unwords ws)] 
+    Reversal {}   -> map (Reversal ind) (parseWordplay ws)
+    HiddenWord {} -> [HiddenWord ind (unwords ws)]
+    _             -> []
+  | otherwise = []
+    where 
+      pair = find (\x -> (unwords $ fst x) `elem` inds) (split2M arg)
+
+twoArgs :: [String] -> [String] -> ParseTree -> [ParseTree]
+twoArgs inds arg typ = case typ of 
+  Insertion ["Insert"] _ _ -> concatMap (\(a, ind, a') -> 
+    [Insertion ind t1 t2 | t1 <- parseWordplay a, t2 <- parseWordplay a']) ins
+  Insertion ["Envelope"] _ _ -> concatMap (\(a, ind, a') -> 
+    [Insertion ind t2 t1 | t1 <- parseWordplay a, t2 <- parseWordplay a']) ins
+  Charade ["Before"] _ _ -> concatMap (\(a, ind, a') -> 
+    [Charade ind t1 t2 | t1 <- parseWordplay a, t2 <- parseWordplay a']) chd
+  Charade ["After"] _ _ -> concatMap (\(a, ind, a') -> 
+    [Charade ind t2 t1 | t1 <- parseWordplay a, t2 <- parseWordplay a']) ins
+  _ -> []
+  where 
+    ins = filter (\(_, y, _) -> not (null y) && (unwords y) `elem` inds) (split3 arg)
+    chd = filter (\(_, y, _) -> null y || (unwords y) `elem` inds) (split3 arg)
 
 parseAnagram :: [String] -> [ParseTree]
-parseAnagram
-  = const []
+parseAnagram ws = oneArg anagramIndicators ws (Anagram [] "")
 
 parseReversal :: [String] -> [ParseTree]
-parseReversal
-  = const []
+parseReversal ws = oneArg reversalIndicators ws (Reversal [] (Synonym ""))
 
 parseInsertion :: [String] -> [ParseTree]
-parseInsertion
-  = const []
+parseInsertion ws = twoArgs insertionIndicators ws (Insertion ["Insert"] (Synonym "") (Synonym ""))
+  ++ twoArgs envelopeIndicators ws (Insertion ["Envelope"] (Synonym "") (Synonym ""))
 
 parseCharade :: [String] -> [ParseTree]
-parseCharade 
-  = const []
+parseCharade ws = twoArgs beforeIndicators ws (Charade ["Before"] (Synonym "") (Synonym ""))
+  ++ twoArgs afterIndicators ws (Charade ["After"] (Synonym "") (Synonym ""))
+
+parseHiddenWord :: [String] -> [ParseTree]
+parseHiddenWord ws = oneArg hiddenWordIndicators ws (HiddenWord [] "")
 
 -- Given...
 parseClue :: Clue -> [Parse]
@@ -104,12 +142,14 @@ parseClue clue@(s, n)
   = parseClueText (words (cleanUp s))
 
 parseClueText :: [String] -> [Parse]
-parseClueText
-  = undefined
+parseClueText ws = concatMap (\(def, link, wp) -> [(def, link, pTree) | pTree <- parseWordplay wp]) filtered 
+  where 
+    filtered = filter (\(x, y, _) -> not (null (synonyms (unwords x))) && (unwords y) `elem` linkWords) (split3M ws)
 
 solve :: Clue -> [Solution]
-solve 
-  = undefined
+solve clue@(s, n) = [(clue, p, ans) | p <- ps, ans <- nub $ evaluate p n]
+  where 
+    ps = parseClue clue 
 
 
 ------------------------------------------------------
